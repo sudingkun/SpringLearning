dba91df368291bef3bb72fb729027a26b0ef6423
WE180 <971204jqx>
2019-08-15 10:01:46 +0800

    springboot 配置使用druid starter配置数据库连接池

---
+++ SpringBoot-Druid/src/main/java/com/we/springboot/druid/DruidApplication.java
@@ -0,0 +1,17 @@
+package com.we.springboot.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * druid的配置，全部由配置文件完成
+ * @author sudingkun
+ */
+@SpringBootApplication
+public class DruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApplication.class, args);
+    }
+
+}
