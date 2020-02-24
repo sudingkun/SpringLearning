e34cdb4c1779cebf604a601580b8f04c8ee020a4
we <1149650559@qq.com>
2020-02-24 21:14:19 +0800

    授权 demo 记录

---
+++ SpringBoot-Oauth/README.md
@@ -0,0 +1,9 @@
+### 参考 https://github.com/justauth/justauth-spring-boot-starter
+
+#### 1、可以使用 nginx 代理，到时候打包放在服务器上
+
+#### 2、这里使用的是 nps 内网穿透。因为当时在调试本地开发比较方便。恰逢远程办公，搭建了 nps。
+
+#### 3、nps 创建客户端，然后配置域名解析
+
+#### nps搭建  http://note.youdao.com/noteshare?id=22315291ca35e1ad6b87e9fe25e4335c
---
+++ SpringBoot-Oauth/src/main/java/com/we/springboot/oauth/OauthApplication.java
@@ -0,0 +1,16 @@
+package com.we.springboot.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author we
+ */
+@SpringBootApplication
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+}
