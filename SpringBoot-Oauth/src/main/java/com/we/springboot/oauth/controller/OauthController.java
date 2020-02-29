819e3f43d9cbf1c2a833bb21d8ee3037a2bba5a1
we <1149650559@qq.com>
2020-02-29 15:17:49 +0800

    add github and qq

--- SpringBoot-Oauth/src/main/java/com/we/springboot/oauth/controller/OauthController.java
+++ SpringBoot-Oauth/src/main/java/com/we/springboot/oauth/controller/OauthController.java
@@ -9,31 +9,49 @@ import me.zhyd.oauth.model.AuthResponse;
 import me.zhyd.oauth.request.AuthRequest;
 import me.zhyd.oauth.utils.AuthStateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 第三方登录 Controller
+ *
  * @author we
  */
 @Slf4j
-@RestController
-@RequestMapping("/oauth")
+@Controller
+@RequestMapping("oauth")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class OauthController {
 
     private final AuthRequestFactory factory;
 
-    @GetMapping
-    public List<String> list() {
-        return factory.oauthList();
+    @GetMapping("{type}")
+    public String index(@PathVariable String type, Model model) {
+        model.addAttribute("type", type);
+        return "index";
     }
+
+
+    /**
+     * 登录类型
+     */
+    @GetMapping
+    @ResponseBody
+    public Map<String, String> list() {
+        List<String> oauthList = factory.oauthList();
+        return oauthList.stream().collect(Collectors.toMap(oauth -> oauth.toLowerCase() + "登录", oauth -> "http://localhost/oauth/login/" + oauth.toLowerCase()));
+    }
+
 
     @GetMapping("login/{type}")
     public void login(@PathVariable String type, HttpServletResponse response) throws IOException {
@@ -41,6 +59,7 @@ public class OauthController {
         response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
     }
 
+    @ResponseBody
     @RequestMapping("{type}/callback")
     public AuthResponse login(@PathVariable String type, AuthCallback callback) {
         AuthRequest authRequest = factory.get(type);
@@ -48,6 +67,5 @@ public class OauthController {
         log.info("【response】= {}", JSONUtil.toJsonStr(response));
         return response;
     }
-
 
 }
