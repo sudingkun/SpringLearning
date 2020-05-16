24a8d57136bcea053e30c4050cd05f653f254635
we <1149650559@qq.com>
2020-05-16 17:32:06 +0800

    新引入了 swagger-bootstrap-ui

--- SpringBoot-Swagger/src/main/java/com/we/springboot/swagger/SwaggerApplication.java
+++ SpringBoot-Swagger/src/main/java/com/we/springboot/swagger/SwaggerApplication.java
@@ -1,12 +1,20 @@
 package com.we.springboot.swagger;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author we
+ * 新引入了 Knife4j，配置的时候和正常的 swagger2 一样
+ * 访问 http://ip:port/doc.html
+ * https://doc.xiaominfo.com/knife4j/
  */
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 @EnableSwagger2
 @SpringBootApplication
 public class SwaggerApplication {
